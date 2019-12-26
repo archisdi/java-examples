56f85b4873bea52b01bf16a189ec6bb22a4303a1
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-12-26 22:06:15 +0700

    add variable lesson

---
+++ 01-variables/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Primitives
+        int age = 24;
+        double androidShare = 81.7;
+        boolean isMarried = true;
+        char grade = 'a';
+
+        // Object
+        String name = "archie";
+    }
+}
