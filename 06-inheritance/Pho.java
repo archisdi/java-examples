591cf980284041baa66bbe9315af6609a6d60287
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-12-28 09:48:20 +0700

    add inheritance

---
+++ 06-inheritance/Pho.java
@@ -0,0 +1,5 @@
+public class Pho extends Noodle {
+    public Pho() {
+        super(30.0, 0.64, "flat", "rice flour");
+    }
+}
---
+++ 06-inheritance/Spaghetti.java
@@ -0,0 +1,5 @@
+public class Spaghetti extends Noodle {
+    public Spaghetti() {
+        super(10.0, 0.50, "curly", "flour");
+    }
+}
